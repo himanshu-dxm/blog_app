a9d0a249c4d555e80be7f67dc4fe7da44e3e7ad1
himanshu <himanhu.behl@gmail.com>
2021-07-19 22:56:08 +0530

    Changed admin home ui

--- lib/screens/admin/admin_home.dart
+++ lib/screens/admin/admin_home.dart
@@ -1,13 +1,95 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_blog/utils/CommonUtils.dart';
 
-class AdminHome extends StatelessWidget {
+class AdminHome extends StatefulWidget {
   const AdminHome({Key? key}) : super(key: key);
 
   @override
+  _AdminHomeState createState() => _AdminHomeState();
+}
+
+class _AdminHomeState extends State<AdminHome> {
+
+  late String authorName,title;
+  @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: CommonUtils.myAppBar("Admin","Home"),
+      appBar: AppBar(
+        backgroundColor: Colors.transparent,
+        // elevation: 0.0,
+        title: Row(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            Text(
+              "Admin",
+              style: TextStyle(
+                color: Colors.amber,
+                fontSize: 22,
+              ),
+            ),
+            Text(
+              "Home",
+              style: TextStyle(
+                color: Colors.pink,
+                fontSize: 22,
+              ),
+            ),
+          ],
+        ),
+        centerTitle: true,
+        actions: <Widget>[
+          InkWell(
+            onTap: () {
+              //TODO Signout system
+            },
+            child: Container(
+              padding: EdgeInsets.symmetric(horizontal: 16),
+              child: Icon(Icons.logout_rounded),
+            ),
+          )
+        ],
+      ),
+      body: Container(
+        child: Column(
+          children: [
+            SizedBox(height: 10,),
+            Container(
+              child: Column(
+                children: [
+                  SizedBox(height: 10,),
+                  Container(
+                    // padding: EdgeInsets.all(6),
+                    height: 200,
+                    width: MediaQuery.of(context).size.width*0.97,
+                    decoration: BoxDecoration(
+                      color: Colors.white24,
+                      borderRadius: BorderRadius.circular(6),
+                    ),
+                    child: Center(
+                      child: Icon(Icons.add_a_photo_rounded,color: Colors.yellowAccent,),
+                    ),
+                  ),
+                  SizedBox(height: 30,),
+                  Container(
+                    padding: EdgeInsets.symmetric(horizontal: 10),
+                    child: Column(
+                      children: [
+                        TextField(
+                          decoration: InputDecoration(hintText: "Name"),
+                          onChanged: (val) {
+                            authorName = val;
+                            print(authorName);
+                          },
+                        ),
+                      ],
+                    ),
+                  ),
+                ],
+              ),
+            ),
+          ],
+        ),
+      ),
     );
   }
 }
--- lib/screens/admin/admin_login.dart
+++ lib/screens/admin/admin_login.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_blog/screens/admin/admin_home.dart';
 import 'package:flutter_blog/utils/CommonUtils.dart';
 
 class AdminLogin extends StatefulWidget {
@@ -13,6 +14,22 @@ class _AdminLoginState extends State<AdminLogin> {
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: CommonUtils.myAppBar("Admin","Login"),
+      body: Container(
+        child: Column(
+          // mainAxisAlignment: MainAxisAlignment.center,
+          crossAxisAlignment: CrossAxisAlignment.center,
+          children: [
+            ElevatedButton(
+                onPressed: () {
+                  Navigator.pushReplacement(
+                    context,
+                      MaterialPageRoute(builder: (context) => AdminHome())
+                  );
+                },
+                child: Text("Login"))
+          ],
+        ),
+      ),
     );
   }
 }
